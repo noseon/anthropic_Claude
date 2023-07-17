e2912da1c950a10d2b566cadc592ea7f6f640add
noseon <fake.on@gmail.com>
2023-07-17 14:53:12 -0300

    Update AnthropicClaude.py

--- src/AnthropicClaude.py
+++ src/AnthropicClaude.py
@@ -2,7 +2,6 @@ import requests
 import json
 import os
 import uuid
-# ------------------------------------------------------------------------------
 uuid_organization = ''
 uuid_conversation = ''
 cookie = ''
@@ -10,11 +9,7 @@ link = 'https://claude.ai/api/'
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0'
 header_b = {"User-Agent": f"{user_agent}", "Referer": "https://claude.ai/chat",
     "Origin": "https://claude.ai", "Content-Type": "application/json", "Accept-Language": "en-US,en;q=0.5"}
-# ------------------------------------------------------------------------------
-
-
 class Anthropic():
-     # ------------------------------------------------------------------------------
      def load_cookie():
           global cookie
           with open(f'{os.getcwd()}/claude.json', "r", encoding="utf-8") as file:
@@ -23,15 +18,12 @@ class Anthropic():
                   for item in data:
                       if item.get("name") == "sessionKey":
                           cookie = item.get("value")
-      # ------------------------------------------------------------------------------
-
       def generate_uuid():
           random_uuid = uuid.uuid4()
           random_uuid_str = str(random_uuid)
           formatted_uuid = f"{random_uuid_str[0:8]}-{random_uuid_str[9:13]}-{random_uuid_str[14:18]}-{random_uuid_str[19:23]}-{random_uuid_str[24:]}"
           return formatted_uuid
-      # ------------------------------------------------------------------------------
-
+          
       def create_new_chat():
           global link, uuid_organization, uuid_conversation,  cookie, user_agent, header_b
           payload = json.dumps({"uuid": Anthropic.generate_uuid(), "name": ""})
@@ -39,7 +31,6 @@ class Anthropic():
           headers.update(header_b)
           requests.request(
                 "POST", f"{link}organizations/{uuid_organization}/chat_conversations", headers=headers, data=payload)
-      # ------------------------------------------------------------------------------
 
       def get_organization_id():
           global link, uuid_organization, uuid_conversation,  cookie, user_agent, header_b
@@ -48,7 +39,6 @@ class Anthropic():
           response = requests.get(f"{link}organizations", headers=headers)
           data = response.json()
           uuid_organization = data[0]['uuid']
-      # ------------------------------------------------------------------------------
 
       def list_conversation():
           global link, uuid_organization, uuid_conversation,  cookie, user_agent, header_b
@@ -62,7 +52,6 @@ class Anthropic():
           except Exception as e:
               Anthropic.create_new_chat()
               Anthropic.list_conversation()
-      # ------------------------------------------------------------------------------
 
       def send_message(prompt):
           global link, uuid_organization, uuid_conversation,  cookie, user_agent, header_b
@@ -92,7 +81,6 @@ class Anthropic():
               return {"answer": json.loads(data[6:])['completion']}['answer']
           except Exception as e:
               return "Erro: Pergunta Besta!!!"
-      # ------------------------------------------------------------------------------
 
       def delete_conversation():
           global link, uuid_organization, uuid_conversation,  cookie, user_agent, header_b
@@ -101,4 +89,3 @@ class Anthropic():
           headers.update(header_b)
           requests.request(
                 "DELETE", f"{link}organizations/{uuid_organization}/chat_conversations/{uuid_conversation}", headers=headers, data=payload)
-#-----------------------------------------------------------------------------
