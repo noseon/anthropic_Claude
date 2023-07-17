1ccab961545e6e7c84c2d97de838975f4a218224
noseon <fake.on@gmail.com>
2023-07-17 17:06:31 -0300

    Update core.py

--- core.py
+++ core.py
@@ -95,4 +95,3 @@ class Anthropic():
           headers.update(header_b)
           requests.request("DELETE", f"{link}organizations/{uuid_organization}/chat_conversations/{uuid_conversation}", headers=headers, data=payload)
 #------------------------------------------------------------------------------
-print(Anthropic.send_message_direct("quero suco de uva como fazer ?"))
